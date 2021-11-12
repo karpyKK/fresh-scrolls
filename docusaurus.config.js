57723a1bed8d955f6ac215725051e1a111877f62
Ivan Zaytsev <36398642+oceanEcho@users.noreply.github.com>
2021-11-12 22:31:47 +0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Fresh Scrolls",
-  url: "https://your-docusaurus-test-site.com",
+  url: "https://oceanecho.github.io/fresh-scrolls/",
   baseUrl: "/fresh-scrolls/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
